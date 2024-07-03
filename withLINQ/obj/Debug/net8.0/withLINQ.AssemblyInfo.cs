991af83a53e8ab9c55e9f1cd8bc3bd66d7aad153
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-07-03 14:41:41 -0300

    Why every time i run this code i have 12 files changed? C# and its problems

--- withLINQ/obj/Debug/net8.0/withLINQ.AssemblyInfo.cs
+++ withLINQ/obj/Debug/net8.0/withLINQ.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("withLINQ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fac77c98b4f07ccc36da9f9133bb66d34469f54c")]
 [assembly: System.Reflection.AssemblyProductAttribute("withLINQ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("withLINQ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
